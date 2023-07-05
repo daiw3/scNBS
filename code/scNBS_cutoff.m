fc53f85ec408b0269958a7e26b36e711260d6a1c
Wei Dai <wd278@login1.mccleary.ycrc.yale.edu>
2023-07-05 19:25:01 -0400

    Update bugs for scNBS_cutoff.m

--- code/scNBS_cutoff.m
+++ code/scNBS_cutoff.m
@@ -36,41 +36,51 @@ function [cut_best_pos, cut_best_neg, pos_edges, neg_edges] = scNBS_cutoff(mat_n
     %% Fowardly Find Cut-offs for Positive signals
     t_max=NaN(1,1);
     t_cut=[];
-    for cut=1:size(pos_rank, 1)
-        mattmp = mat_net(pos_rank(1:cut,2),pos_rank(1:cut, 3),sample1);
-        Xtmp = zeros([size(mattmp, 3), 1]);
-        for i = 1:size(mattmp, 3)
-            Xtmp(i,:) = mean(diag(mattmp(:,:,i)));
+    if size(pos_rank, 1) == 0
+        cut_best_pos = [];
+        pos_edges = [];
+    else
+        for cut=1:size(pos_rank, 1)
+            mattmp = mat_net(pos_rank(1:cut,2),pos_rank(1:cut, 3),sample1);
+            Xtmp = zeros([size(mattmp, 3), 1]);
+            for i = 1:size(mattmp, 3)
+                Xtmp(i,:) = mean(diag(mattmp(:,:,i)));
+            end
+            [t_cut(cut),p_cut] = corr(Xtmp, y);
+            if (abs(t_cut(cut)) > t_max) | (isnan(t_max))
+                t_max = t_cut(cut);
+                pvalue = p_cut;
+                cut_best = cut;
+            end
         end
-        [t_cut(cut),p_cut] = corr(Xtmp, y);
-        if (abs(t_cut(cut)) > t_max) | (isnan(t_max))
-            t_max = t_cut(cut);
-            pvalue = p_cut;
-            cut_best = cut;
-        end
+        
+        cut_best_pos = 1:cut_best;
+        pos_edges = pos_rank(cut_best_pos,:);
     end
+
     
-    cut_best_pos = 1:cut_best;
-    pos_edges = pos_rank(cut_best_pos,:);
     
      %% Fowardly Find Cut-offs for Negative signals
     t_max=NaN(1,1);
     t_cut = [];
-    for cut=1:size(neg_rank, 1)
-        mattmp = mat_net(neg_rank(1:cut,2),neg_rank(1:cut, 3),sample1);
-        Xtmp = zeros([size(mattmp, 3), 1]);
-        for i = 1:size(mattmp, 3)
-            Xtmp(i,:) = mean(diag(mattmp(:,:,i)));
+    if(size(neg_rank, 1) == 0)
+        cut_best_neg = [];
+        neg_edges = [];
+    else
+        for cut=1:size(neg_rank, 1)
+            mattmp = mat_net(neg_rank(1:cut,2),neg_rank(1:cut, 3),sample1);
+            Xtmp = zeros([size(mattmp, 3), 1]);
+            for i = 1:size(mattmp, 3)
+                Xtmp(i,:) = mean(diag(mattmp(:,:,i)));
+            end
+            [t_cut(cut),p_cut] = corr(Xtmp, y);
+            if (abs(t_cut(cut)) > t_max) | (isnan(t_max))
+                t_max = abs(t_cut(cut));
+                pvalue = p_cut;
+                cut_best = cut;
+            end
         end
-        [t_cut(cut),p_cut] = corr(Xtmp, y);
-        if (abs(t_cut(cut)) > t_max) | (isnan(t_max))
-            t_max = abs(t_cut(cut));
-            pvalue = p_cut;
-            cut_best = cut;
-        end
+        cut_best_neg = 1:cut_best;
+        neg_edges = neg_rank(cut_best_neg,:);
     end
-    
-    cut_best_neg = 1:cut_best;
-    neg_edges = neg_rank(cut_best_neg,:);
-    
 end
